3b315dd3bc3ad4f717192ac44f32906121b22da5
Seteemio <68556559+seteemio@users.noreply.github.com>
2021-02-01 19:06:34 +0100

    correcting a function call in app.js

--- JSh00t2/src/app.js
+++ JSh00t2/src/app.js
@@ -73,6 +73,6 @@ function gameLoop(timeStamp) {
     heroTouched();
     scoreShow();
     livesShow();
-    explosionShow();
+    explosionDisplay();
     window.requestAnimationFrame(gameLoop);
 }
