7d0d13262000401a62528fe4ab5aa6e4423b5f54
Seteemio <68556559+seteemio@users.noreply.github.com>
2021-02-09 06:32:20 +0100

    Update game.js
    Added some functions.
    some older funcions were modified.

--- JSh00t2/src/game.js
+++ JSh00t2/src/game.js
@@ -16,16 +16,18 @@ function heroDisplay() {
         hero.update();
     }
 }
-function shootDisplay() {
-    for (let i = 0; i < bulletFired.length; ++i) {
-        if (bulletFired[i].shot) {
-            bulletFired[i].draw();
-            bulletFired[i].update();
+function shootDisplay(bullet) {
+    for (let i = 0; i < bullet.length; ++i) {
+        if (bullet[i].shot) {
+            bullet[i].draw();
+            bullet[i].update();
+        } else if (bullet[i] === false) {
+            bullet[i].y = canvas.width * 10;
         }
     }
 }
-function isShooting() {
-    shootDisplay();
+function heroIsShooting() {
+    shootDisplay(bulletFired);
     if (key.space) {
         if (shotToggle == false) {
             shotToggle = true;
@@ -40,9 +42,24 @@ function isShooting() {
             }
             window.setTimeout(function () {
                 shotToggle = false;
-            }, 200);
+            }, 150);
         }
     }
+}
+function scoring() {
+    killForRun++;
+    score += multiplier;
+    scoreForLives += multiplier;
+    if (Math.trunc(killForRun / 5) < 1) {
+        multiplier = 1;
+    } else {
+        multiplier = Math.trunc(killForRun / 5) + 1;
+    }
+    if (scoreForLives >= 2000) {
+        scoreForLives = 0;
+        ++lives;
+    }
+
 }
 function enemyShot() {
     for (let i = 0; i < enemy.length; ++i) {
@@ -50,26 +67,19 @@ function enemyShot() {
             if (isColliding(bulletFired[j], enemy[i])) {
                 if (bulletFired[j].shot) {
                     kills++;
-                    killForRun++;
-                    score += multiplier;
-                    scoreForLives += multiplier;
-                    if (Math.trunc(killForRun / 5) < 1) {
-                        multiplier = 1;
-                    } else {
-                        multiplier = Math.trunc(killForRun / 5) + 1;
-                    }
-                    if (scoreForLives >= 2000) {
-                        scoreForLives = 0;
-                        ++lives;
-                    }
+                    scoring();
                     explosionShow(enemy[i]);
-                    enemy[i].x = randomize(50, canvas.width - 50);
+                    enemy[i].x = xEnemyCreation.x;
                     enemy[i].y = 0;
-                    if (enemy[i].xSpeed < 50) {
+                    enemy[i].pattern = randomize(1, 3);
+                    if (enemy[i].xSpeed < 30) {
                         enemy[i].xSpeed++;
                     }
-                    if (enemy[i].ySpeed < 3) {
+                    if (enemy[i].ySpeed < 6) {
                         enemy[i].ySpeed += 0.2;
+                    }
+                    if (xEnemyCreation.xDirection < -20 || xEnemyCreation.xDirection > 20) {
+                        xEnemyCreation.xDirection *= 1.05;
                     }
                     bulletFired[j].shot = false;
                 }
@@ -80,22 +90,14 @@ function enemyShot() {
 function heroTouched() {
     for (let i = 0; i < enemy.length; ++i) {
         if (isColliding(hero, enemy[i])) {
-            lives--;
-            multiplier = 1;
-            killForRun = 0;
-            blink = true;
-            explosionShow(hero)
-            hero.x = -100000;
-            hero.y = -100000;
-            gameover2.message = 'Total Kills =' + kills;
-            gameover2.str = gameover2.message.split();
+            heroDie();
             if (lives > 0) {
                 window.setTimeout(function () {
                     blink = false;
                     hero.x = initX;
                     hero.y = initY;
                     for (let j = 0; j < enemy.length; ++j) {
-                        enemy[j].x = randomize(50, canvas.width - 50)
+                        enemy[j].x = randomize(limit, canvas.width - limit)
                         enemy[j].y = 0;
                         enemy[j].xSpeed = 10;
                         enemy[j].ySpeed = 1;
@@ -128,8 +130,8 @@ function explosionShow(param) {
     for (let i = 0; i < explosion.length; ++i) {
         if (explosion[i].exist === false) {
             explosion[i].exist = true;
-            explosion[i].x = param.x;
-            explosion[i].y = param.y;
+            explosion[i].x = param.x + (param.xSize / 2);
+            explosion[i].y = param.y + (param.ySize / 2);
             explosion[i].snd.play();
             break;
         }
@@ -146,7 +148,7 @@ function toggleKey(keyPressed, bool) {
 }
 function textShow(param) {
     param.draw();
-    setInterval(function () { param.update(); }, 100);
+    param.update();
 }
 function beginDisplay() {
     textShow(begin1);
@@ -156,4 +158,146 @@ function gameoverDisplay() {
     textShow(gameover1);
     textShow(gameover2);
     textShow(gameover3);
+}
+function backgroundRadial() { //useless
+    let x, y;
+    x = -50;
+    y = -50
+    let radial = context.createRadialGradient(x, y, 50, 200, 150, 775);
+    radial.addColorStop(0, '#AAAA88');
+    radial.addColorStop(0.25, 'black');
+    context.beginPath();
+    context.fillStyle = radial;
+    context.arc(x, y, 335, 0, 2 * Math.PI);
+    context.fill();
+}
+function bossShot() {
+    for (let i = 0; i < bulletFired.length; ++i) {
+        if (isColliding(bulletFired[i], boss)) {
+            if (boss.pattern) {
+                if (boss.life > 0) {
+                    boss.life--;
+                    explosionShow(bulletFired[i]);
+                    scoring();
+                }
+                if (boss.life == 0) { kills++; }
+                bulletFired[i].shot = false;
+                bulletFired[i].x = canvas.width * 10;
+            }
+        }
+    }
+}
+function bossShootCollideHero() {
+    for (let i = 0; i < bulletBoss.length; ++i) {
+        if (isColliding(bulletBoss[i], hero)) {
+            if (boss.life >= 0) {
+                explosionShow(bulletBoss[i]);
+                bulletBoss[i].shot = false;
+                bulletBoss[i].x = canvas.width * 10;
+                heroDie();
+                if (lives > 0) {
+                    window.setTimeout(function () {
+                        blink = false;
+                        hero.x = initX;
+                        hero.y = initY;
+                        boss.x = (canvas.width / 2) - (boss.xSize / 2);
+                        boss.y = 0 - (boss.ySize / 2);
+                        boss.pattern = false;
+                        boss.xSpeed = 10;
+                        boss.ySpeed = 0.5;
+                        boss.xUpdate = randomize(1, 5);
+                        boss.yUpdate = 0.5;
+                        boss.yDirection = 0.5;
+                        boss.xDirection = 0.5;
+                        boss.alive = true;
+                        boss.life = bossLife;
+                    }, 1000);
+                }
+            }
+        }
+    }
+}
+function bossIsShooting() {
+    shootDisplay(bulletBoss);
+    if (bossShotToggle == false) {
+        bossShotToggle = true;
+        for (let i = 0; i < bulletBoss.length; ++i) {
+            if (bulletBoss[i].shot === false) {
+                bulletBoss[i].x = (boss.x + (boss.xSize / 2) - (bulletBoss[i].xSize / 2));
+                bulletBoss[i].y = boss.y;
+                bulletBoss[i].shot = true;
+                bulletBoss[i].snd.play();
+                break;
+            }
+        }
+        window.setTimeout(function () {
+            bossShotToggle = false;
+        }, 500);
+    }
+}
+function bossDisplay() {
+    if (boss.y < canvas.height + boss.ySize) {
+        bossShot();
+        bossDie();
+        bossShootCollideHero();
+        if (boss.pattern) { bossIsShooting(); }
+        boss.draw();
+        boss.update();
+        boss.hpDraw();
+        if (isColliding(hero, boss)) {
+            if (boss.life >= 0) {
+                heroDie();
+                if (lives > 0) {
+                    window.setTimeout(function () {
+                        blink = false;
+                        hero.x = initX;
+                        hero.y = initY;
+                        boss.x = (canvas.width / 2) - (boss.xSize / 2);
+                        boss.y = 0 - (boss.ySize / 2);
+                        boss.pattern = false;
+                        boss.xSpeed = 10;
+                        boss.ySpeed = 0.5;
+                        boss.xUpdate = randomize(1, 5);
+                        boss.yUpdate = 0.5;
+                        boss.yDirection = 0.5;
+                        boss.xDirection = 0.5;
+                        boss.alive = true;
+                        boss.life = bossLife;
+                    }, 1000);
+                }
+            }
+        }
+
+    }
+}
+function bossDie() {
+    let switchBoom = false;
+    if (boss.alive === false) {
+        if (switchBoom === false) {
+            switchboom = true;
+            window.setInterval(function () {
+                switchBoom = false;
+                explosionShow(boss);
+            }, 100);
+        }
+    }
+}
+function heroDie() {
+    lives--;
+    multiplier = 1;
+    killForRun = 0;
+    blink = true;
+    explosionShow(hero);
+    hero.x = -100000;
+    hero.y = -100000;
+    gameover2.message = 'Total Kills =' + kills;
+    gameover2.str = gameover2.message.split();
+}
+function callOfTheBoss() {
+    if (kills > 75 ) {
+        bossDisplayToggle = true;
+        for (let i = 0; i < enemy.length; ++i) {
+            enemy[i].ySpeed *= -1.5;
+        }
+    }
 }
